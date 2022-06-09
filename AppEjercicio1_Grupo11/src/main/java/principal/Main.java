c8aa3e42e209802fc7e6805a2790a069365bd8ef
franmadoeryy <francisco.madoery@sarsy-srl.com>
2022-06-09 18:10:29 -0300

    Ejercicio 1 terminado
    ¿? creo que estaría terminado

--- AppEjercicio1_Grupo11/src/main/java/principal/Main.java
+++ AppEjercicio1_Grupo11/src/main/java/principal/Main.java
@@ -1,13 +1,21 @@
 package principal;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import entidad.Usuario;
 import negocio.UsuarioNegocio;
 
 public class Main {
 
+	static ApplicationContext appContext;
+	
 	public static void main(String[] args) {
 		
-		Usuario usuario = new Usuario();
+		appContext = new ClassPathXmlApplicationContext("Resources/Beans.xml");
+		
+		Usuario usuario = (Usuario) appContext.getBean("usuario");
 	    usuario.setUsuario("Pepe");
 	    usuario.setContrasenia("123");
 	    
@@ -17,6 +25,7 @@ public class Main {
 	    	System.out.println("Se agrego correctamente");
 	    else
 	    	System.out.println("No se pudo agregar, el usuario ya existe en la BD");
-		    	
+		   
+	    ((ConfigurableApplicationContext)(appContext)).close();
 	}	
 }
