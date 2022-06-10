19b1c20a072e16c4870991f7d8a0e537e995a72a
franmadoeryy <francisco.madoery@sarsy-srl.com>
2022-06-09 23:31:15 -0300

    Extras ejercicio2

--- AppEjercicio2_Grupo11/src/main/java/principal/Main.java
+++ AppEjercicio2_Grupo11/src/main/java/principal/Main.java
@@ -11,20 +11,40 @@ import resources.Config;
 
 public class Main {
 	
+	static ApplicationContext appContext;
+	
 	public static void main(String[] args) {
 		
-		ApplicationContext appContext = new AnnotationConfigApplicationContext(Config.class);
+		appContext = new AnnotationConfigApplicationContext(Config.class);
 		
-		Usuario usuario = (Usuario)appContext.getBean("usuario");
+		//Los atributos se completaron en el XML
+		//usuario.setUsuario("Pepe");
+	    //usuario.setContrasenia("123");
+
+		//Obtener usuarioArturo objeto que retorna el Bean
+		Usuario usuarioArturo = (Usuario) appContext.getBean("usuarioArturo");
+		//actualizo el password del objeto desde el main con el set
+		usuarioArturo.setContrasenia("#"+usuarioArturo.getContrasenia()+"#");
+		
+		//Obtener usuarioArturo objeto que retorna el Bean
+		Usuario usuarioJose = (Usuario) appContext.getBean("usuarioJose");
+		//actualizo el password del objeto desde el main con el set
+		usuarioJose.setContrasenia("#"+usuarioJose.getContrasenia()+"#");  
+		
+		
+		UsuarioNegocio usuarioNegocio = (UsuarioNegocio) appContext.getBean("usuarioNegocio");
+		boolean estadoArturo= usuarioNegocio.agregarUsuario(usuarioArturo);
+		boolean estadoJose= usuarioNegocio.agregarUsuario(usuarioJose);
+		if(estadoArturo)
+			System.out.println("Se agrego correctamente a Arturo");
+		else
+			System.out.println("No se pudo agregar a Arturo, el usuario ya existe en la BD");
 	    
-	    UsuarioNegocio usuarioNegocio = (UsuarioNegocio)appContext.getBean("usuarioNegocio");
-	    boolean estado = usuarioNegocio.agregarUsuario(usuario);
-	    
-	    if(estado)
-	    	System.out.println("Se agrego correctamente");
+		if(estadoJose)
+	    	System.out.println("Se agrego correctamente a Jose");
 	    else
-	    	System.out.println("No se pudo agregar, el usuario ya existe en la BD");
-	    
+	    	System.out.println("No se pudo agregar a Jose, el usuario ya existe en la BD");
+		   
 	    ((ConfigurableApplicationContext)(appContext)).close();
 	}	
 }
--- AppEjercicio2_Grupo11/src/main/java/resources/Config.java
+++ AppEjercicio2_Grupo11/src/main/java/resources/Config.java
@@ -1,7 +1,9 @@
 package resources;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import dao.UsuarioDao;
 import entidad.Usuario;
@@ -9,23 +11,36 @@ import negocio.UsuarioNegocio;
 
 @Configuration
 public class Config {
-	
-	@Bean
-	public Usuario usuario() {
-		Usuario usuario = new Usuario();
-		usuario.setUsuario("Ramiro");
-		usuario.setContrasenia("ElRami123");
-		return usuario;
-	}
-	
-	@Bean
-	public UsuarioDao usuarioDao() {
-		return new UsuarioDao();
-	}
-	
-	@Bean
-	public UsuarioNegocio usuarioNegocio() {
-		return new UsuarioNegocio();
+
+	@Bean(initMethod="initUsuario", destroyMethod="destroyUsuario")
+	@Scope(value= ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public Usuario usuarioArturo() {
 		
+		Usuario usu = new Usuario();
+		usu.setUsuario("ArturoIllia");
+		usu.setContrasenia("Arturo@123");
+		
+		return usu;
+	}
+	
+	@Bean(initMethod="initUsuario", destroyMethod="destroyUsuario")
+	@Scope(value= ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public Usuario usuarioJose() {
+		
+		return new Usuario("JoseLuisPerales", "Jose@123");
+	}
+	
+	@Bean
+	@Scope(value= ConfigurableBeanFactory.SCOPE_SINGLETON)
+	public UsuarioNegocio usuarioNegocio() {
+		
+		return new UsuarioNegocio();
+	}
+	
+	@Bean
+	@Scope(value= ConfigurableBeanFactory.SCOPE_SINGLETON)
+	public UsuarioDao usuarioDao() {
+		
+		return new UsuarioDao();
 	}
 }
